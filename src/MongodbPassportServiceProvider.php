d3d04f561a43ea23febdd3d2f115a319e161a3fe
Steve Porter <steve@designmynight.com>
2018-01-22 21:41:11 +0000

    fix: fixes designmynight/laravel-mongodb-passport#3
    Use PHP's class alias if the AliasLoader does not exist

--- src/MongodbPassportServiceProvider.php
+++ src/MongodbPassportServiceProvider.php
@@ -15,10 +15,17 @@ class MongodbPassportServiceProvider extends ServiceProvider
         /*
          * Passport client extends Eloquent model by default, so we alias them.
          */
-        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-        $loader->alias('Laravel\Passport\AuthCode', AuthCode::class);
-        $loader->alias('Laravel\Passport\Client', Client::class);
-        $loader->alias('Laravel\Passport\PersonalAccessClient', PersonalAccessClient::class);
-        $loader->alias('Laravel\Passport\Token', Token::class);
+        if (class_exists('Illuminate\Foundation\AliasLoader')) {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('Laravel\Passport\AuthCode', AuthCode::class);
+            $loader->alias('Laravel\Passport\Client', Client::class);
+            $loader->alias('Laravel\Passport\PersonalAccessClient', PersonalAccessClient::class);
+            $loader->alias('Laravel\Passport\Token', Token::class);
+        } else {
+            class_alias('Laravel\Passport\AuthCode', AuthCode::class);
+            class_alias('Laravel\Passport\Client', Client::class);
+            class_alias('Laravel\Passport\PersonalAccessClient', PersonalAccessClient::class);
+            class_alias('Laravel\Passport\Token', Token::class);
+        }
     }
 }
