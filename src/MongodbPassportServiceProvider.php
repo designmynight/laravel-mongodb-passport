8eb2d52435338bcc17b56bd541a17a0a35b53581
Fabián Gonzalo Artur de la Villarmois <gonzartur@gmail.com>
2021-11-29 11:50:19 +1300

    Add ClientCommand

--- src/MongodbPassportServiceProvider.php
+++ src/MongodbPassportServiceProvider.php
@@ -2,14 +2,16 @@
 
 namespace DesignMyNight\Mongodb;
 
+use Illuminate\Console\Command;
 use Illuminate\Support\ServiceProvider;
 use DesignMyNight\Mongodb\Passport\AuthCode;
+use DesignMyNight\Mongodb\Console\ClientCommand;
 use DesignMyNight\Mongodb\Passport\Bridge\RefreshTokenRepository;
 use DesignMyNight\Mongodb\Passport\Client;
 use DesignMyNight\Mongodb\Passport\PersonalAccessClient;
-use DesignMyNight\Mongodb\Passport\RefreshToken;
 use DesignMyNight\Mongodb\Passport\Token;
 use Laravel\Passport\Bridge\RefreshTokenRepository as PassportRefreshTokenRepository;
+use Laravel\Passport\Console\ClientCommand as PassportClientCommand;
 use Laravel\Passport\Passport;
 
 class MongodbPassportServiceProvider extends ServiceProvider
@@ -26,6 +28,10 @@ class MongodbPassportServiceProvider extends ServiceProvider
 
         $this->app->bind(PassportRefreshTokenRepository::class, function () {
             return $this->app->make(RefreshTokenRepository::class);
+        });
+
+        $this->app->extend(PassportClientCommand::class, function () {
+            return new ClientCommand();
         });
     }
 }
