a2779ee6b8671d5bf8904572f8190e2d7cdb874d
Geidson Benicio Coelho <geidsonc@gmail.com>
2023-03-31 10:54:31 -0300

    Adiciona o comando PurgeCommand e resolve modelo RefreshToken

--- src/Passport/Bridge/RefreshTokenRepository.php
+++ src/Passport/Bridge/RefreshTokenRepository.php
@@ -4,6 +4,7 @@ namespace Sysvale\Mongodb\Passport\Bridge;
 
 use Laravel\Passport\Bridge\RefreshTokenRepository as BaseRefreshTokenRepository;
 use Laravel\Passport\Events\RefreshTokenCreated;
+use Laravel\Passport\Passport;
 use League\OAuth2\Server\Entities\RefreshTokenEntityInterface;
 
 /**
@@ -17,7 +18,7 @@ class RefreshTokenRepository extends BaseRefreshTokenRepository
      */
     public function getNewRefreshToken()
     {
-        return new RefreshToken();
+        return Passport::refreshToken();
     }
 
     /**
